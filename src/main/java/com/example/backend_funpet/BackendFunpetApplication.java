1e304a8c29d0f92054e64b207528606f6d722022
NDQuynh25 <QuynhND.B21CN646@stu.ptit.edu.vn>
2024-06-27 00:53:01 +0700

    first commit

---
+++ src/main/java/com/example/backend_funpet/BackendFunpetApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_funpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFunpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFunpetApplication.class, args);
+	}
+
+}
